e0c1675435d148d574f7b0d0997636aa421beaad
yaron-cider <100203813+yaron-cider@users.noreply.github.com>
2023-05-17 10:44:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import sys
 
 print("hello world")
+
+jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
